638e1d3c867c181e284400fd3b3c9124f0e54b78
Hồ Thái Khanh <hothaikhanh@gmail.com>
2022-11-07 23:42:53 +0700

    Xây dựng phần slider #1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -50,9 +50,34 @@ const landingPage = {
             }, 100);
         }
     },
+    sliderStart: function () {
+        let slidersCount = 4;
+        let sliders = Array.from(Array(slidersCount).keys());
 
+        let leftBtn = $(".slider__nav--left");
+        let rightBtn = $(".slider__nav--right");
+        let slidersPage = $$(".slider__page");
+
+        leftBtn.addEventListener("click", () => {
+            sliders.unshift(sliders.pop());
+            slidersPage[sliders[0]].classList.add("slider__page--active");
+            slidersPage[sliders[1]].classList.remove("slider__page--active");
+        });
+        rightBtn.addEventListener("click", () => {
+            sliders.push(sliders.shift());
+            slidersPage[sliders[sliders.length - 1]].classList.remove("slider__page--active");
+            slidersPage[sliders[0]].classList.add("slider__page--active");
+        });
+
+        // setInterval(function () {
+        //     sliders.push(sliders.shift());
+        //     slidersPage[sliders[sliders.length - 1]].classList.remove("slider__page--active");
+        //     slidersPage[sliders[0]].classList.add("slider__page--active");
+        // }, 8000);
+    },
     start: function () {
         this.menuStart();
+        this.sliderStart();
     },
 };
 
