422c4cad304f9d2dafc83f8de02732c37d4515c5
Hồ Thái Khanh <hothaikhanh@gmail.com>
2023-03-07 00:26:24 +0700

    Phát triển phần Item Slider

--- src/app/slider-item/slider-item.component.ts
+++ src/app/slider-item/slider-item.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-slider-item',
   templateUrl: './slider-item.component.html',
   styleUrls: ['./slider-item.component.scss']
 })
-export class SliderItemComponent {
+export class SliderItemComponent implements OnInit {
+  @Input() link: string = ''
+  @Input() picture: string = ''
+  @Input() name: string = ""
+  @Input() price_old?: number
+  @Input() price_current: number = 0
+  @Input() ratings: number = 0
+  @Input() ID: string = ""
 
+  starsCount: number = this.ratings;
+  starSet = "";
+  renderStars() {
+    for (let i = 0; this.starsCount >= 1; this.starsCount--) {
+
+      this.starSet += `<div class="items-slider__star star--full"></div>`;
+    }
+    if (this.starsCount > 0) {
+      this.starSet += `<div class="items-slider__star star--half"></div>`;
+    }
+  }
+
+  ngOnInit(): void {
+    this.renderStars()
+  }
 }
--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,12 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import data from "src/ITEMS.json"
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit {
+
+
+
   @Input() slider_title: string = "";
   @Input() items: any;
+
+  itemsList: any = [];
+
+  getItemsList() {
+    for (let i = 0; i < data.length; i++) {
+      if (this.items.includes(data[i].ID)) {
+        this.itemsList.push(data[i])
+      }
+    }
+    console.log(this.itemsList);
+
+  }
+
+  ngOnInit(): void {
+    this.getItemsList()
+  }
+
+
 
 }
